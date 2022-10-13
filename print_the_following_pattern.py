c3d51d695a3d3f1a145ff4f0be563f4b027df155
AjaykumarGunti <111426854+AjaykumarGunti@users.noreply.github.com>
2022-10-13 19:32:51 +0530

    a=int(input()) for row in range(a):     for col in range(a):         if row==col or row+col==a-1 :             print("x",end='')         else:             print("0",end='')     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,8 +1,8 @@
 a=int(input())
 for row in range(a):
     for col in range(a):
-        if row==col:
-            print("0",end="")
+        if row==col or row+col==a-1 :
+            print("x",end='')
         else:
-            print("x",end="")
+            print("0",end='')
     print()
